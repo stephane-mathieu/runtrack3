336121da5fc3d29bac8b4d86dbcd3391df711b10
stephane-mathieu <mathieu.stephane@laplateforme.io>
2022-03-17 17:41:42 +0100

    job03

--- Jour 04/Job03/index.php
+++ Jour 04/Job03/index.php
@@ -4,15 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js"></script>
+    <script type='text/javascript' src='script.js'></script>
     <title>Document</title>
 </head>
 <body>
-    <input id="id"type="text" placeholder="id">
-    <input id="nom"type="text" placeholder="nom">
-    <select  id="select"class="form-select col-md-12 " aria-label="Default select example" name="droits" id="droits">
-  
+
+    <select name="type" id="type">
+
     </select>
-    <button id="button" type="button">filter</button>
+
+    <button id='filter'> Filtrer </button>
+
+    <ul>
+
+    </ul>
 </body>
 </html>
--- Jour 04/Job03/script.js
+++ Jour 04/Job03/script.js
@@ -1,30 +1,63 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const url = 'pokemon.json';
+"use strict ";
+document.addEventListener('DOMContentLoaded', (event) => {
 
-    let selected = document.getElementById("select")
+    let btn = document.getElementById('filter')
+    let select = document.querySelector('select')
+    let ul = document.querySelector('ul')
+    let li = document.querySelector('li')
+    let allLi = document.querySelectorAll('li')
+    let typeList = []
 
-    let typelist = [];
 
-    fetch(url)
-        .then(response => response.json())
-        .then(response => {
-            for (var i = 0; i < response.length; i++) {
-                let sans_doublons = response[i].type;
+    fetch('pokemon.json')
+        .then((response) => response.json())
+        .then((response) => {
 
-                typelist.push(sans_doublons.toString());
-
+            /*display option select*/
+            for (let i = 0; i < response.length; i++) {
+                let typePokemon = response[i].type
+                typeList.push(typePokemon.toString())
             }
-            let filteredArray = typelist.filter(function(ele, pos) {
-                return typelist.indexOf(ele) == pos;
+
+            /*Retirer les doublons*/
+            let newList = typeList.filter(function(ele, pos) {
+                return typeList.indexOf(ele) == pos;
             })
 
-            for (var i = 0; i < filteredArray.length; i++) {
-                let option = document.createElement("option");
-                option.innerHTML = filteredArray[i]
-                selected.append(option);
+            /* Display Select no doublons */
+            for (let i = 0; i < newList.length; i++) {
+                let NewOpt = document.createElement('option')
+                NewOpt.innerHTML = newList[i]
+                select.append(NewOpt)
             }
 
+            btn.addEventListener("click", function() {
+                let type = document.querySelector('option')
+                let choice = select.selectedIndex;
+                let valeur = select.options[choice].value;
+                let texte = select.options[choice].text;
+                console.log(texte)
+                for (let i = 0; i < response.length; i++) {
+                    if (texte == response[i].type) {
+                        console.log(select)
+                        console.log(response[i].type)
+                        let NewLi = document.createElement('li')
+                        NewLi.innerHTML = response[i].id + ' ' + response[i].name.french + ' ' + response[i].type
+                        ul.append(NewLi)
+                        if (ul) {
+                            ul.remove()
+                        }
+                    } else if (type == '-Selectionner un type-') {
+                        console.log(type)
+                        console.log('pas de type')
+                        let NewLi = document.createElement('li')
+                        NewLi.innerHTML = response[i].id + ' ' + response[i].name.french + ' ' + response[i].type
+                        ul.append(NewLi)
+                    }
 
+                }
+            })
+        })
+        .catch((error) => console.log(error))
 
-        });
 })
