989dec7f1981958f85533a7b3c5a8d69911b09f6
stephane-mathieu <mathieu.stephane@laplateforme.io>
2022-03-21 13:21:05 +0100

    jour06

---
+++ Jour 06/Job01/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+</body>
+</html>
